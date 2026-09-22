305474bc27422ad2a13e4bd25ddd9053b89eec19
leandromateus028-create <leandromateus028@gmail.com>
2026-09-22 20:26:03 +0000

    Inicio da agenda no terminal

---
+++ agenda.js
@@ -0,0 +1,8 @@
+let nomeCliente = "Leandro"; 
+let horarioEscolhido = "10:00";
+let horarios = ["09:00","10:00", "11:00","13:00","14:00" ];
+
+
+console.log(nomeCliente);
+console.log(horarioEscolhido);
+
